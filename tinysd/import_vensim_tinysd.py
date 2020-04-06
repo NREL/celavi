144a9a8fa97b39bc860323d51f20403a0a9942ee
Annika Eberle <annika.eberle@nrel.gov>
2020-04-06 17:51:02 -0600

    Adds timer to see how long tiny-sd takes to run in Python

--- tinysd/import_vensim_tinysd.py
+++ tinysd/import_vensim_tinysd.py
@@ -1,16 +1,21 @@
 import pysd
 import matplotlib.pyplot as plt
+import time
 
 # Import model (or read original Vensim model)
 # model = pysd.read_vensim('tiny-sd copy.mdl')  # use for first load of model from Vensim
+
+start = time.time()
 model = pysd.load('tiny-sd_pysd_v30mar2020.py')
 
 # Show model documents after import to confirm it loaded
-print(model.doc())
+# print(model.doc())
 
 # Run model and collect the result
 # other stocks: 'Raw Material Extraction'
 result = model.run(return_columns=['relative landfill', 'Products at End of Life', 'Product Remanufacture', 'Material Recycle', 'Product Reuse', 'Landfill and Incineration', 'Products in Use'])
+end = time.time()
+print("Run time (s):", round(end - start, 2))
 
 # Plot results
 plt.figure(1, figsize=(5, 8))
