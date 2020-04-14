0e1870446a34313d183a9b0886476c89e1290474
Alicia Key <akey7@users.noreply.github.com>
2020-04-14 11:34:01 -0600

    Issue #8
    Make a class for the app.

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import pysd
+import networkx as nx
+import pandas as pd
+
+
+class App:
+    def __init__(self, model_fn):
+        self.model = pysd.load(model_fn)
+
+    def run(self):
+        print("Hello world")
+
+
 if __name__ == '__main__':
-    print("Hello world")
+    app = App("tinysd/tiny-sd_pysd_v30mar2020.py")
+    app.run()
