8d147141cfa2b618d2ade6289e9fa55d132474a2
Alicia Key <akey7@users.noreply.github.com>
2020-06-02 16:47:33 -0600

    Issue #21
    Remove the absolute path names from the model.

--- tinysd/import-national-scale.py
+++ tinysd/import-national-scale.py
@@ -2,10 +2,10 @@ import pysd
 import matplotlib.pyplot as plt
 
 # Read in Vensim model: national scale, adjusted for import
-model = pysd.read_vensim("C://Users//rhanes//Documents//GitHub//tiny-lca//tinysd//vensim model//national-scale//natl-wind-importable.mdl")  # use for first load of model from Vensim
+# model = pysd.read_vensim("C://Users//rhanes//Documents//GitHub//tiny-lca//tinysd//vensim model//national-scale//natl-wind-importable.mdl")  # use for first load of model from Vensim
 
 # load model if not converting afresh
-model = pysd.load("C://Users//rhanes//Documents//GitHub//tiny-lca//tinysd//vensim model//national-scale//natl-wind-importable.py")
+model = pysd.load("tinysd/vensim model/national-scale/natl-wind-importable.py")
 
 # Show model documents after import to confirm it loaded
 # print(model.doc())
@@ -14,19 +14,19 @@ model = pysd.load("C://Users//rhanes//Documents//GitHub//tiny-lca//tinysd//vensi
 result = model.run()
 
 # Plot results
-# plt.figure(1, figsize=(5, 8))
-# plt.subplot(211)
-# stock_names = ['Products at End of Life', 'Product Remanufacture',
-#                'Material Recycle', 'Product Reuse', 'Landfill and Incineration',
-#                'Products in Use']
-# stocks = result[stock_names]
-# plt.plot(stocks)
-# plt.ylabel('Mass (kg)')
-# plt.xlabel('Months')
-#
-# plt.subplot(212)
-# relative_landfill = result['relative landfill']
-# plt.plot(relative_landfill)
-# plt.ylabel('Relative Landfill')
-# plt.xlabel('Months')
-# plt.show()
+plt.figure(1, figsize=(5, 8))
+plt.subplot(211)
+stock_names = ['Products at End of Life', 'Product Remanufacture',
+               'Material Recycle', 'Product Reuse', 'Landfill and Incineration',
+               'Products in Use']
+stocks = result[stock_names]
+plt.plot(stocks)
+plt.ylabel('Mass (kg)')
+plt.xlabel('Months')
+
+plt.subplot(212)
+relative_landfill = result['relative landfill']
+plt.plot(relative_landfill)
+plt.ylabel('Relative Landfill')
+plt.xlabel('Months')
+plt.show()
