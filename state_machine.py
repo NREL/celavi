4f2bbb06d489d2c62ef899ad922783588db70af5
Alicia Key <akey7@users.noreply.github.com>
2020-06-10 13:15:21 -0600

    Issue #25
    Put letters in the titles to use with making a caption for the figure.

--- state_machine.py
+++ state_machine.py
@@ -37,44 +37,44 @@ def report(result):
 
 
 def make_plots(report_df, context):
-    fig, axs = plt.subplots(nrows=4, ncols=2, figsize=(7, 7))
+    fig, axs = plt.subplots(nrows=4, ncols=2, figsize=(10, 7))
     plt.tight_layout()
     plt.subplots_adjust(top=0.95, hspace=0.5)
 
     xs = report_df["ts"].values
 
-    axs[0, 0].set_title("Discrete Fraction Use")
+    axs[0, 0].set_title("(a) Discrete Fraction Use")
     axs[0, 0].set_ylim(-0.1, 1.1)
     axs[0, 0].plot(xs, report_df["fraction_use"])
 
-    axs[1, 0].set_title("Discrete Fraction Remanufacture")
+    axs[1, 0].set_title("(c) Discrete Fraction Remanufacture")
     axs[1, 0].set_ylim(-0.1, 1.1)
     axs[1, 0].plot(xs, report_df["fraction_remanufacture"])
 
-    axs[2, 0].set_title("Discrete Fraction Recycle")
+    axs[2, 0].set_title("(e) Discrete Fraction Recycle")
     axs[2, 0].set_ylim(-0.1, 1.1)
     axs[2, 0].plot(xs, report_df["fraction_recycle"])
 
-    axs[3, 0].set_title("Discrete Fraction Landfill")
+    axs[3, 0].set_title("(g) Discrete Fraction Landfill")
     axs[3, 0].set_ylim(-0.1, 1.1)
     axs[3, 0].plot(xs, report_df["fraction_landfill"])
 
-    axs[0, 1].set_title("SD Fraction Reus(ing)")
+    axs[0, 1].set_title("(b) SD Fraction Reus(ing)")
     axs[0, 1].set_ylim(-0.1, 1.1)
     axs[0, 1].plot(np.arange(len(context.fraction_reuse)), \
                    context.fraction_reuse, c="r")
 
-    axs[1, 1].set_title("SD Fraction Remanufactur(ing)")
+    axs[1, 1].set_title("(d) SD Fraction Remanufactur(ing)")
     axs[1, 1].set_ylim(-0.1, 1.1)
     axs[1, 1].plot(np.arange(len(context.fraction_remanufacture)), \
                    context.fraction_remanufacture, c="r")
 
-    axs[2, 1].set_title("SD Fraction Recycl(ing)")
+    axs[2, 1].set_title("(f) SD Fraction Recycl(ing)")
     axs[2, 1].set_ylim(-0.1, 1.1)
     axs[2, 1].plot(np.arange(len(context.fraction_remanufacture)),
                    context.fraction_recycle, c="r")
 
-    axs[3, 1].set_title("Discrete Fraction Landfill(ing)")
+    axs[3, 1].set_title("(h) Discrete Fraction Landfill(ing)")
     axs[3, 1].set_ylim(-0.1, 1.1)
     axs[3, 1].plot(np.arange(len(context.fraction_remanufacture)),
                    context.fraction_landfill, c="r")
