6ddc6e41f72a7761c0243e36007d747dbc17464b
Alicia Key <akey7@users.noreply.github.com>
2020-07-21 14:45:16 -0600

    Issue #43
    Move the code that starts the state machine to `__main__.py`

--- celavi/__main__.py
+++ celavi/__main__.py
@@ -1,38 +1,101 @@
-import sys
+from celavi.state_machine_units_model import Context
+from typing import List, Dict
+import pandas as pd
 import os
-
-import pysd
 import matplotlib.pyplot as plt
-import time
+import random
+import numpy as np
 
-start = time.time()
-model = pysd.load('sd_model.py')
 
-# Show model documents after import to confirm it loaded
-print(model.doc())
+def report(result):
+    max_timestep = int(result["ts"].max())
+    report_list: List[Dict] = []
+    for ts in range(1, max_timestep + 1):
+        timestep_result = result[result["ts"] == ts]
+        number_use = len(timestep_result[timestep_result["state"] == "use"])
+        number_recycle = len(
+            timestep_result[timestep_result["state"] == "recycle"]
+        )
+        number_remanufacture = len(
+            timestep_result[timestep_result["state"] == "remanufacture"]
+        )
+        number_landfill = len(
+            timestep_result[timestep_result["state"] == "landfill"]
+        )
+        total_component_materials = len(timestep_result)
+        report_list.append(
+            {
+                "ts": ts,
+                "total_components": total_component_materials,
+                "fraction_use": number_use / total_component_materials,
+                "fraction_recycle": number_recycle / total_component_materials,
+                "fraction_remanufacture": number_remanufacture / total_component_materials,
+                "fraction_landfill": number_landfill / total_component_materials,
+            }
+        )
+    report_df = pd.DataFrame(report_list)
+    return report_df
 
-# Run model and collect the result
-# other stocks: 'Raw Material Extraction'
-result = model.run(return_columns=['relative landfill', 'Products at End of Life', 'Product Remanufacture', 'Material Recycle', 'Product Reuse', 'Landfill and Incineration', 'Products in Use'])
-end = time.time()
-print("Run time (s):", round(end - start, 2))
 
-# Plot results
-plt.figure(1, figsize=(5, 8))
-plt.subplot(211)
-stock_names = ['Products at End of Life', 'Product Remanufacture',
-               'Material Recycle', 'Product Reuse', 'Landfill and Incineration',
-               'Products in Use']
-stocks = result[stock_names]
-plt.plot(stocks)
-plt.ylabel('Mass (kg)')
-plt.xlabel('Months')
+def make_plots(report_df, context):
+    fig, axs = plt.subplots(nrows=4, ncols=2, figsize=(10, 7))
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.93, bottom=0.07, hspace=0.7)
 
-plt.subplot(212)
-relative_landfill = result['relative landfill']
-plt.plot(relative_landfill)
-plt.ylabel('Relative Landfill')
-plt.xlabel('Months')
-plt.show()
+    xs = report_df["ts"].values
 
-print(sys.path[0])
+    axs[0, 0].set_title("(a) Discrete Fraction Use")
+    axs[0, 0].set_ylim(-0.1, 1.1)
+    axs[0, 0].plot(xs, report_df["fraction_use"])
+
+    axs[1, 0].set_title("(c) Discrete Fraction Remanufacture")
+    axs[1, 0].set_ylim(-0.1, 1.1)
+    axs[1, 0].plot(xs, report_df["fraction_remanufacture"])
+
+    axs[2, 0].set_title("(e) Discrete Fraction Recycle")
+    axs[2, 0].set_ylim(-0.1, 1.1)
+    axs[2, 0].plot(xs, report_df["fraction_recycle"])
+
+    axs[3, 0].set_title("(g) Discrete Fraction Landfill")
+    axs[3, 0].set_ylim(-0.1, 1.1)
+    axs[3, 0].set_xlabel("discrete timestep")
+    axs[3, 0].plot(xs, report_df["fraction_landfill"])
+
+    axs[0, 1].set_title("(b) SD Fraction Reus(ing)")
+    axs[0, 1].set_ylim(-0.1, 1.1)
+    axs[0, 1].plot(np.arange(len(context.fraction_reuse)), \
+                   context.fraction_reuse, c="r")
+
+    axs[1, 1].set_title("(d) SD Fraction Remanufactur(ing)")
+    axs[1, 1].set_ylim(-0.1, 1.1)
+    axs[1, 1].plot(np.arange(len(context.fraction_remanufacture)), \
+                   context.fraction_remanufacture, c="r")
+
+    axs[2, 1].set_title("(f) SD Fraction Recycl(ing)")
+    axs[2, 1].set_ylim(-0.1, 1.1)
+    axs[2, 1].plot(np.arange(len(context.fraction_remanufacture)),
+                   context.fraction_recycle, c="r")
+
+    axs[3, 1].set_title("(h) Discrete Fraction Landfill(ing)")
+    axs[3, 1].set_ylim(-0.1, 1.1)
+    axs[3, 1].set_xlabel("discrete timestep")
+    axs[3, 1].plot(np.arange(len(context.fraction_remanufacture)),
+                   context.fraction_landfill, c="r")
+
+    plt.show()
+
+
+def run_and_report():
+    print(os.getcwd())
+    context = Context("natl-wind-importable.py", year_intercept=1980.0, years_per_timestep=0.25)
+    context.populate_components("long_ten_usgs_dataset_materials.csv")
+    material_component_log = context.run()
+    report_df = report(material_component_log)
+    make_plots(report_df, context)
+    print("writing material component log...")
+    material_component_log.to_csv("material_component_log.csv", index=False, float_format='%.3f')
+
+
+if __name__ == "__main__":
+    random.seed(0)
+    run_and_report()
