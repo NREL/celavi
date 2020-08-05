b1acca85512ff0604fad840b73bbde7b36abc37a
Alicia Key <akey7@users.noreply.github.com>
2020-08-04 19:53:47 -0600

    Issue #56
    New state transition diagram is in place and tested.

--- celavi/tests/unit/test_component_material.py
+++ celavi/tests/unit/test_component_material.py
@@ -14,3 +14,51 @@ def a_component_material():
 
 def test_material_type(a_component_material):
     assert a_component_material.material_type == "Aluminum"
+
+
+def test_use_landfill(a_component_material):
+    a_component_material.state = "use"
+    a_component_material.transition("landfilling")
+    assert a_component_material.state == "landfill"
+
+
+def test_use_reuse(a_component_material):
+    a_component_material.state = "use"
+    a_component_material.transition("reusing")
+    assert a_component_material.state == "reuse"
+
+
+def test_use_recycle(a_component_material):
+    a_component_material.state = "use"
+    a_component_material.transition("recycling")
+    assert a_component_material.state == "recycle"
+
+
+def test_use_remanufacture(a_component_material):
+    a_component_material.state = "use"
+    a_component_material.transition("remanufacturing")
+    assert a_component_material.state == "remanufacture"
+
+
+def test_use_recycle(a_component_material):
+    a_component_material.state = "use"
+    a_component_material.transition("recycling")
+    assert a_component_material.state == "recycle"
+
+
+def test_reuse_landfill(a_component_material):
+    a_component_material.state = "reuse"
+    a_component_material.transition("landfilling")
+    assert a_component_material.state == "landfill"
+
+
+def test_recycle_manufacture(a_component_material):
+    a_component_material.state = "recycle"
+    a_component_material.transition("manufacturing")
+    assert a_component_material.state == "manufacture"
+
+
+def test_remanufacture_use(a_component_material):
+    a_component_material.state = "remanufacture"
+    a_component_material.transition("using")
+    assert a_component_material.state == "use"
