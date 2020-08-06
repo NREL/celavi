9bb6d93267137a62ad43bc7c44aff87b795652e3
Alicia Key <akey7@users.noreply.github.com>
2020-08-05 19:48:57 -0600

    Issue #56
    Test the landfill increment.

--- celavi/tests/unit/test_landfill_virgin_materials.py
+++ celavi/tests/unit/test_landfill_virgin_materials.py
@@ -22,7 +22,19 @@ def a_dummy_context_with_component_material():
     return dummy_context, component_material
 
 
-def test_landfill_virgin_material(a_dummy_context_with_component_material):
+def test_virgin_material(a_dummy_context_with_component_material):
     dummy_context, component_material = a_dummy_context_with_component_material
     component_material.transition("landfilling", 1)
     assert dummy_context.virgin_material_inventory.materials["Aluminum"] == -1
+
+
+def test_virgin_material(a_dummy_context_with_component_material):
+    dummy_context, component_material = a_dummy_context_with_component_material
+    component_material.transition("landfilling", 1)
+    assert dummy_context.virgin_material_inventory.materials["Aluminum"] == -1
+
+
+def test_landfill_material(a_dummy_context_with_component_material):
+    dummy_context, component_material = a_dummy_context_with_component_material
+    component_material.transition("landfilling", 1)
+    assert dummy_context.landfill_material_inventory.materials["Aluminum"] == 1
