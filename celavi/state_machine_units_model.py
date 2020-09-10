540be78a3a6cdfa97fec6ee8cd61310edc6821cc
Alicia Key <akey7@users.noreply.github.com>
2020-09-10 17:41:04 -0600

    Issue #85
    Refactor the Unique identifier class into its own module.

--- celavi/state_machine_units_model.py
+++ celavi/state_machine_units_model.py
@@ -7,26 +7,7 @@ import simpy  # type: ignore
 from csv import DictWriter
 
 from .inventory import Inventory
-
-
-class UniqueIdentifier:
-    _unique_identifier_integer = 0
-
-    @classmethod
-    def unique_identifier(cls) -> int:
-        """
-        This returns a UUID that can serve as a unique identifier for anything.
-        These UUIDs can be used as keys in a set or hash if needed.
-
-        TODO: Replace this with an incrementing integer.
-
-        Returns
-        -------
-        str
-            A UUID to be used as a unique identifier.
-        """
-        cls._unique_identifier_integer += 1
-        return cls._unique_identifier_integer
+from .unique_identifier import UniqueIdentifier
 
 
 @dataclass(frozen=True)
