160c9ad64d7e33d4b7ae23659b61006f64c689b0
rjhanes <rebecca.hanes@nrel.gov>
2020-10-06 14:09:21 -0600

    updates function calls

--- tinysd/natl-wind-run.py
+++ tinysd/natl-wind-run.py
@@ -181,10 +181,10 @@ blade_lbd_with_rd = model.run(params={'scenario name':'blade_lbd_with_rd',
                                       'recycle research annual cost reduction':0.003})
 
 
-## Stepwise model running
+## Stepwise model running with default parameter settings
 test_model = get_model(pysdfile)
 
-first_step_results = first_step(test_model, init_time=1982, timestep=0.25, scen_name='test-case')
+first_step_results = first_step(test_model, init_time=1982, timestep=0.25, param_dict={'scenario name':'test-case'})
 
 second_step_results = step(test_model, time=first_step_results[1], timestep=0.25)
 
@@ -193,16 +193,31 @@ third_step_results = step(test_model, time=second_step_results[1], timestep=0.25
 new_model = reset_model(test_model, pysdfile)
 
 
-## Stepwise model running in a loop
+## Stepwise model running in a loop with a parameter value updated at every timestep
 start_time=1982
 final_time=1992
 quarters=0.25
 
+# load pySD model
 loop_model=get_model(pysdfile)
 
-first = first_step(loop_model, init_time=start_time, timestep=quarters, scen_name='loop-case')
+# initialize parameter value
+strat_val=0
+
+# initialize model by running the first time step
+first = first_step(loop_model, init_time=start_time, timestep=quarters, param_dict={'scenario name':'loop-case'})
+
+# get the current model time from function output
 model_time = first[1]
 
 for i in np.arange(start_time+quarters, final_time+quarters, quarters):
-    step_results = step(loop_model,time=model_time,timestep=quarters)
+
+    # wait for DES input to set parameter value
+
+    step_results = step(loop_model,time=model_time,timestep=quarters,
+                        param_dict={'remanufactured material strategic value':strat_val})
+
     model_time += quarters
+    strat_val += 1
+
+    # send output to DES model for use at next timestep
