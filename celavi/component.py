d5d310216a28316fd6feeb8894ee424cb87ba6d6
Alicia Key <akey7@users.noreply.github.com>
2021-03-22 16:11:40 -0600

    Update component.py
    Create a lifecycle queue based on the input from the CostGraph

--- celavi/component.py
+++ celavi/component.py
@@ -54,8 +54,9 @@ class Component:
             The year in which this component enters the use state for the first
             time.
 
-        lifespan_timesteps: float
-            The lifespan, in timesteps, of the component. The argument can be
+        intitial_lifespan_timesteps: float
+            The lifespan, in timesteps, of the component during its first
+            In use phase. The argument can be
             provided as a floating point value, but it is converted into an
             integer before it is assigned to the instance attribute. This allows
             more intuitive integration with random number generators defined
@@ -65,7 +66,7 @@ class Component:
             The total mass of the component, in tonnes.
         """
 
-        self.phase = ""  # There is no location
+        self.phase = ""  # There is no location initially
         self.context = context
         self.id = UniqueIdentifier.unique_identifier()
         self.kind = kind
@@ -73,225 +74,8 @@ class Component:
         self.ylat = ylat
         self.year = year
         self.mass_tonnes = mass_tonnes
-        self.lifespan_timesteps = int(lifespan_timesteps)  # timesteps
-        self.transitions_table = self.make_transitions_table()
-        self.pathway: Deque[Tuple[str, int]] = None
-
-    def make_transitions_table(self) -> Dict[StateTransition, NextState]:
-        """
-        This method should be called once during instantiation of a component.
-
-        It creates the table of states, transitions, and next states.
-
-        Both recycling and landfilling have long lifetimes. For an explanation
-        of why, see their state entry functions.
-
-        Returns
-        -------
-        Dict[StateTransition, NextState]
-            A dictionary mapping current states and transitions (the key) into
-            the next state (the value). See the documentation for the
-            StateTransition and NextState classes.
-        """
-        transitions_table = {
-            StateTransition(state="use", transition="landfilling"): NextState(
-                state="landfill",
-                lifespan_min=1000,
-                lifespan_max=1000,
-                state_entry_function=self.landfill,
-                state_exit_function=self.leave_use,
-            ),
-            StateTransition(state="use", transition="recycling_to_raw"): NextState(
-                state="recycle_to_raw",
-                lifespan_min=1000,
-                lifespan_max=1000,
-                state_entry_function=self.recycle_to_raw,
-                state_exit_function=self.leave_use,
-            ),
-            StateTransition(state="use", transition="recycling_to_clinker"): NextState(
-                state="recycle_to_clinker",
-                lifespan_min=1000,
-                lifespan_max=1000,
-                state_entry_function=self.recycle_to_clinker,
-                state_exit_function=self.leave_use,
-            )
-        }
-
-        return transitions_table
-
-    @staticmethod
-    def recycle_to_raw(context, component, timestep: int) -> None:
-        """
-        Recycles a component by incrementing the material in recycling storage.
-
-        Currently, there is no corresponding leave_recycle because only other
-        processes deduct from the recycling inventory (e.g., manufacturing)
-
-        Parameters
-        ----------
-        context: Context
-            The context in which this component lives. There is no type
-            in the method signature to prevent a circular dependency.
-
-        component: Component
-            The component which is being landfilled.
-
-        timestep: int
-            Current model timestep
-        """
-        _yield = context.cost_params['fine_grind_yield'] * context.cost_params['coarse_grind_yield']
-        _loss = 1 - _yield
-
-        context.recycle_to_raw_component_inventory.increment_quantity(
-            item_name=component.kind, quantity=_yield, timestep=timestep
-        )
-        context.recycle_to_raw_material_inventory.increment_quantity(
-            item_name=component.kind, quantity=_yield*component.mass_tonnes, timestep=timestep
-        )
-        context.landfill_component_inventory.increment_quantity(
-            item_name=component.kind, quantity=_loss, timestep=timestep
-        )
-        context.landfill_material_inventory.increment_quantity(
-            item_name=component.kind, quantity=_loss*component.mass_tonnes, timestep=timestep)
-
-    @staticmethod
-    def recycle_to_clinker(context, component, timestep:int) -> None:
-        """
-        Recycles a component to clinker by incrementing the material in
-        recycling-to-clinker storage.
-
-        Currently, there is no corresponding leave_recycle because only other
-        processes deduct from the recycling inventory (e.g., manufacturing)
-
-        Parameters
-        ----------
-        context: Context
-            The context in which this component lives. There is no type
-            in the method signature to prevent a circular dependency.
-
-        component: Component
-            The component which is being landfilled.
-
-        timestep: int
-            Current model timestep
-        """
-        _yield = context.cost_params['coarse_grind_yield']
-        _loss = 1.0 - _yield
-        context.recycle_to_clinker_component_inventory.increment_quantity(
-            item_name=component.kind, quantity=_yield, timestep=timestep
-        )
-        context.recycle_to_clinker_material_inventory.increment_quantity(
-            item_name=component.kind, quantity=_yield * component.mass_tonnes, timestep=timestep
-        )
-        context.landfill_component_inventory.increment_quantity(
-            item_name=component.kind, quantity=_loss, timestep=timestep
-        )
-        context.landfill_material_inventory.increment_quantity(
-            item_name=component.kind, quantity=_loss*component.mass_tonnes, timestep=timestep)
-
-    @staticmethod
-    def landfill(context, component, timestep:int) -> None:
-        """
-        Landfills a component by incrementing the material in the landfill.
-
-        The landfill process is special because there is no corresponding
-        leave_landfill method since component materials never leave the
-        landfill.
-
-        This is a static method so it can be called by an arbitrary function
-        during a SimPy process. Since it is a static method, it is not
-        attached to any instance of the Component class, so the component
-        must be passed explicitly.
-
-        Parameters
-        ----------
-        context: Context
-            The context in which this component lives. There is no type
-            in the method signature to prevent a circular dependency.
-
-        component: Component
-            The component which is being landfilled.
-
-        timestep:int
-            Current model timestep
-        """
-        # print(f"Landfill process component {component.id}, kind {component.kind} timestep={timestep}")
-        context.landfill_component_inventory.increment_quantity(
-            item_name=component.kind, quantity=1, timestep=timestep,
-        )
-        context.landfill_material_inventory.increment_quantity(
-            item_name=component.kind, quantity=component.mass_tonnes, timestep=timestep,
-        )
-
-    @staticmethod
-    def use(context, component, timestep:int) -> None:
-        """
-        Makes a material enter the use phases from another state.
-
-        This is a static method so it can be called by an arbitrary function
-        during a SimPy process. Since it is a static method, it is not
-        attached to any instance of the Component class, so the component
-        must be passed explicitly.
-
-        Parameters
-        ----------
-        context: Context
-            The context in which this component lives. There is no type
-            in the method signature to prevent a circular dependency.
-
-        component: Component
-            The component which is being landfilled.
-
-        timestep:int
-            Current model timestep
-        """
-        # print(f"Use process component {component.id}, timestep={timestep}")
-        context.use_component_inventory.increment_quantity(
-            item_name=component.kind, quantity=1, timestep=timestep,
-        )
-        context.use_mass_inventory.increment_quantity(
-            item_name=component.kind, quantity=component.mass_tonnes, timestep=timestep,
-        )
-
-        # This will need to be conditional on manufacturing
-        context.virgin_component_inventory.increment_quantity(
-            item_name=component.kind, quantity=-1, timestep=timestep
-        )
-        context.virgin_material_inventory.increment_quantity(
-            item_name=component.kind, quantity=-component.mass_tonnes,
-            timestep=timestep,
-        )
-
-    @staticmethod
-    def leave_use(context, component, timestep:int):
-        """
-        This method decrements the use inventory when a component material leaves use.
-
-        This is a static method so it can be called by an arbitrary function
-        during a SimPy process. Since it is a static method, it is not
-        attached to any instance of the Component class, so the component
-        must be passed explicitly.
-
-        Parameters
-        ----------
-        context: Context
-            The conext in which this component lives
-
-        component: Component
-            The component which is being taken out of use.
-
-        timestep: int
-            Current model timestep
-        """
-        # print(
-        #     f"Leave use process component_material {component.id}, timestep={timestep}"
-        # )
-        context.use_component_inventory.increment_quantity(
-            item_name=component.kind, quantity=-1, timestep=timestep,
-        )
-        context.use_mass_inventory.increment_quantity(
-            item_name=component.kind, quantity=-component.mass_tonnes, timestep=timestep,
-        )
+        self.initial_lifespan_timesteps = int(lifespan_timesteps)  # timesteps
+        self.pathway: Deque[Tuple[str, int]] = deque()
 
     def begin_life(self, env):
         """
@@ -310,8 +94,18 @@ class Component:
         yield env.timeout(begin_timestep)
         path_choice = self.context.cost_graph.choose_paths()
         self.pathway = deque()
-        for phase in path_choice[0]['path']:
-            self.pathway.append(phase)
+
+        # for phase in path_choice[0]['path']:
+        #     self.pathway.append(phase)
+
+        for facility, lifespan in path_choice[0]['path']:
+            if facility.startswith("in use"):
+                self.pathway.append((facility, self.initial_lifespan_timesteps))
+            elif facility.startswith("landfill"):
+                self.pathway.append((facility, self.context.max_timesteps * 2))
+            else:
+                self.pathway.append((facility, lifespan))
+
         env.process(self.eol_process(env))
 
     def transition(self, transition: str, timestep: int) -> None:
@@ -344,7 +138,7 @@ class Component:
             )
         next_state = self.transitions_table[lookup]
         self.state = next_state.state
-        self.lifespan_timesteps = next_state.lifespan
+        self.initial_lifespan_timesteps = next_state.lifespan
         # self.transition_list.append(transition)
         if next_state.state_entry_function is not None:
             next_state.state_entry_function(self.context, self, timestep)
