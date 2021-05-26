54882083919b740fea8aa40ad7909103b72ed6f6
Ghosh, Tapajyoti <Tapajyoti.Ghosh@nrel.gov>
2021-05-26 11:50:05 -0600

    Conflict resolution

--- celavi/__main__.py
+++ celavi/__main__.py
@@ -1,5 +1,7 @@
 import argparse
-from costgraph import CostGraph
+from .routing import Router
+from .costgraph import CostGraph
+from .compute_locations import ComputeLocations
 
 parser = argparse.ArgumentParser(description='Check CELAVI input data')
 parser.add_argument('--locations', help='Path to locations file')
@@ -8,6 +10,20 @@ parser.add_argument('--fac_edges', help='Facility edges file')
 parser.add_argument('--routes', help='Routes file')
 parser.add_argument('--transpo_edges', help='Transportation edges file')
 args = parser.parse_args()
+
+# if compute_locations is enabled (True), compute locations from raw input files (e.g., LMOP, US Wind Turbine Database)
+compute_locations = False
+if compute_locations:
+    args.locations = '../celavi-data/inputs/locations_computed.csv'
+    loc = ComputeLocations()
+    loc.join_facilities(locations_output_file=args.locations)
+
+# if run_routes is enabled (True), compute routing distances between all input locations
+run_routes = False
+if run_routes:
+    routes_computed = Router.get_all_routes(locations_file=args.locations)
+    # reset argument for routes file to use computed routes rather than user input
+    args.routes = '../celavi-data/preprocessing/routes_computed.csv'
 
 netw = CostGraph(
     step_costs_file=args.step_costs,
@@ -18,7 +34,8 @@ netw = CostGraph(
     sc_begin= 'in use',
     sc_end=['landfilling', 'cement co-processing'],
     year=2000.0,
-    max_dist=300.0
+    max_dist=300.0,
+    verbose=2
 )
 
 print(netw.choose_paths(),'\n')
