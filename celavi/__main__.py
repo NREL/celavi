26d7f2a7578bb9ea7e17bc7d240745788e8d4644
rjhanes <rebecca.hanes@nrel.gov>
2021-06-14 14:22:57 -0600

    removes unused use_limited_set boolean and updates comment

--- celavi/__main__.py
+++ celavi/__main__.py
@@ -1,7 +1,7 @@
 import argparse
-from .routing import Router
-from .costgraph import CostGraph
-from .compute_locations import ComputeLocations
+from routing import Router
+from costgraph import CostGraph
+from compute_locations import ComputeLocations
 
 parser = argparse.ArgumentParser(description='Check CELAVI input data')
 parser.add_argument('--locations', help='Path to locations file')
@@ -13,11 +13,11 @@ args = parser.parse_args()
 
 # if compute_locations is enabled (True), compute locations from raw input files (e.g., LMOP, US Wind Turbine Database)
 compute_locations = False
-# Until the step_cost file is updated (or programmatically generated) to include all facility ids, cost graph can't run
-# with the full computed data set; toggle selects list of facilities that can be run based on the facility_id lookup.
-use_limited_set = True
+# Note that the step_cost file must be updated (or programmatically generated)
+# to include all facility ids. Otherwise, cost graph can't run with the full
+# computed data set.
 if compute_locations:
-    args.locations = '../celavi-data/inputs/locations_computed.csv'
+    args.locations = '../../celavi-data/inputs/locations_computed.csv'
     loc = ComputeLocations()
     loc.join_facilities(locations_output_file=args.locations)
 
