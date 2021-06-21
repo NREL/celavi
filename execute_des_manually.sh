1292784c93916a9887c84acf09ce234ab70ace01
Alicia Key <Alicia.Key@nrel.gov>
2021-06-21 10:09:28 -0600

    Create execute_des_manually.sh
    This execute the DES call manually.

---
+++ execute_des_manually.sh
@@ -0,0 +1,10 @@
+python /Users/akey/Projects/tiny-lca/tiny-lca/des-call.py \
+--locations /Users/akey/Projects/celavi-data/celavi-data/inputs/locations.csv \
+--step_costs /Users/akey/Projects/celavi-data/celavi-data/inputs/step_costs.csv \
+--fac_edges /Users/akey/Projects/celavi-data/celavi-data/inputs/fac_edges.csv \
+--routes /Users/akey/Projects/celavi-data/celavi-data/preprocessing/routes.csv \
+--transpo_edges /Users/akey/Projects/celavi-data/celavi-data/inputs/transpo_edges.csv \
+--turbine_data /Users/akey/Projects/celavi-data/celavi-data/inputs/TX_input_data_with_masses.csv \
+--avg_blade_masses /Users/akey/Projects/celavi-data/celavi-data/inputs/avgblademass.csv \
+--lci /Users/akey/Projects/celavi-data/celavi-data/pylca_celavi_data \
+--outputs /Users/akey/Projects/celavi-data/celavi-data/outputs
