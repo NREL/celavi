9be888fa5db8ad8acc6f1b3b170d92c09fb341cd
Alicia Key <Alicia.Key@nrel.gov>
2021-06-28 10:04:09 -0600

    Update des_interface.py
    Remove unnecessary blank lines.

--- celavi/pylca_celavi/des_interface.py
+++ celavi/pylca_celavi/des_interface.py
@@ -43,29 +43,18 @@ postprocessing() writes:
 
 
 def pylca_run_main(df):
-
-    # '''ALICIA replace this part with the dataframe you are sending'''
-    # '''Delete this line after integration'''
-    # df  = pd.read_csv('data_for_lci_test.csv')
-    
     df = df[df['flow quantity'] != 0]
-    
-   
-    
+
     res_df = pd.DataFrame()
     df=df.reset_index()
-    
     for index,row in df.iterrows():
         new_df = df[df['index'] == index]
         year = row['year']
         stage = row['stage']
         material = row['material']
         facility_id = row['facility_id']
-    
-    
-    
-    
-        'Correcting life cycle inventory with material is concrete'
+
+        # Correcting life cycle inventory with material is concrete
         # Calculates the concrete lifecycle flow and emissions inventory
         df_static,df_emissions = concrete_life_cycle_inventory_updater(new_df, year, material, stage)
      
