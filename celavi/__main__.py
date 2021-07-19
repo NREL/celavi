43bf5021e80579ae7d95607c8e5056f88b726a4b
rjhanes <rebecca.hanes@nrel.gov>
2021-07-19 12:42:46 -0600

    sets run_routes to default False and uncomments rest of file

--- celavi/__main__.py
+++ celavi/__main__.py
@@ -105,7 +105,7 @@ else:
     locations = locations_filename
 
 # if run_routes is enabled (True), compute routing distances between all input locations
-run_routes = True
+run_routes = False
 if run_routes:
     routes_computed = Router.get_all_routes(locations_file=locations_filename,
                                             route_pair_file=route_pair_filename,
@@ -118,108 +118,107 @@ if run_routes:
 else:
     routes = routes_filename
 
-# netw = CostGraph(
-#     step_costs_file=step_costs_filename,
-#     fac_edges_file=fac_edges_filename,
-#     transpo_edges_file=transpo_edges_filename,
-#     locations_file=locations_filename,
-#     routes_file=routes_filename,
-#     sc_begin= 'in use',
-#     sc_end=['landfilling', 'cement co-processing'],
-#     year=2000.0,
-#     max_dist=300.0,
-#     verbose=0,
-#     blade_mass=50.0, #@todo update with actual value
-#     finegrind_cumul_initial=1.0,
-#     coarsegrind_cumul_initial=1.0,
-#     finegrind_initial_cost=80.0, #@todo update with actual value
-#     coarsegrind_initial_cost=60.0, #@todo update with actual value
-#     finegrind_learnrate=-0.05,
-#     coarsegrind_learnrate=-0.05
-# )
-#
-# # Create the DES context and tie it to the CostGraph
-# context = Context(
-#     locations_filename=locations_filename,
-#     step_costs_filename=step_costs_filename,
-#     possible_items=["nacelle", "blade", "tower", "foundation"],
-#     cost_graph=netw,
-#     cost_graph_update_interval_timesteps=12,
-#     avg_blade_masses_filename=avg_blade_masses_filename
-# )
-#
-# # Create the turbine dataframe that will be used to populate
-# # the context with components. Repeat the creation of blades
-# # 3 times for each turbine.
-#
-# turbine_data = pd.read_csv(turbine_data_filename)
-# components = []
-# for _, row in turbine_data.iterrows():
-#     year = row['year']
-#     blade_mass_tonnes = row['Glass Fiber:Blade']
-#     foundation_mass_tonnes = row['foundation_mass_tonnes']
-#     facility_id = int(row['facility_id'])
-#     n_turbine = int(row['n_turbine'])
-#
-#     for _ in range(n_turbine):
-#         for _ in range(3):
-#             components.append({
-#                 'year': year,
-#                 'kind': 'blade',
-#                 'mass_tonnes': blade_mass_tonnes,
-#                 'facility_id': facility_id
-#             })
-#
-# components = pd.DataFrame(components)
-#
-# # Create the lifespan functions for the components.
-# np.random.seed(13)
-# timesteps_per_year = 12
-# min_lifespan = 120
-# L = 240
-# K = 2.2
-# lifespan_fns = {
-#     "nacelle": lambda: 30 * timesteps_per_year,
-#     "blade": lambda: 20 * timesteps_per_year,
-#     # "blade": lambda: weibull_min.rvs(K, loc=min_lifespan, scale=L-min_lifespan, size=1)[0],
-#     "foundation": lambda: 50 * timesteps_per_year,
-#     "tower": lambda: 50 * timesteps_per_year,
-# }
-#
-# # Populate the context with components.
-# context.populate(components, lifespan_fns)
-#
-# # Run the context
-# result = context.run()
-#
-# # Output .csv files of the mass flows of each mass inventory.
-# mass_facility_inventories = result["mass_facility_inventories"]
-# for facility_name, facility in mass_facility_inventories.items():
-#     output_filename = os.path.join(subfolder_dict['outputs_folder'], f'{facility_name}.csv')
-#     output_filename = output_filename.replace(' ', '_')
-#     facility.transaction_history.to_csv(output_filename, index_label='timestep')
-#
-# # After PyLCA / DES integration is complete, the next 3 lines should be
-# # eliminated
-# data_for_lci_filename = os.path.join(subfolder_dict['outputs_folder'], 'data_for_lci.csv')
-# data_for_lci_df = pd.DataFrame(context.data_for_lci)
-# data_for_lci_df.to_csv(data_for_lci_filename, index=False)
-#
-# # Plot the cumulative count levels of the inventories
-# count_facility_inventory_items = list(result["mass_facility_inventories"].items())
-# nrows = 5
-# ncols = ceil(len(count_facility_inventory_items) / nrows)
-# fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 10))
-# plt.tight_layout()
-# for i in range(len(count_facility_inventory_items)):
-#     subplot_col = i // nrows
-#     subplot_row = i % nrows
-#     ax = axs[subplot_row][subplot_col]
-#     facility_name, facility = count_facility_inventory_items[i]
-#     cum_hist_blade = facility.cumulative_history["blade"]
-#     ax.set_title(facility_name)
-#     ax.plot(range(len(cum_hist_blade)), cum_hist_blade)
-#     ax.set_ylabel("tonnes")
-# plot_output_path = os.path.join(subfolder_dict['outputs_folder'], 'blade_counts.png')
-# plt.savefig(plot_output_path)
-#
+netw = CostGraph(
+    step_costs_file=step_costs_filename,
+    fac_edges_file=fac_edges_filename,
+    transpo_edges_file=transpo_edges_filename,
+    locations_file=locations_filename,
+    routes_file=routes_filename,
+    sc_begin= 'in use',
+    sc_end=['landfilling', 'cement co-processing'],
+    year=2000.0,
+    max_dist=300.0,
+    verbose=0,
+    blade_mass=50.0, #@todo update with actual value
+    finegrind_cumul_initial=1.0,
+    coarsegrind_cumul_initial=1.0,
+    finegrind_initial_cost=80.0, #@todo update with actual value
+    coarsegrind_initial_cost=60.0, #@todo update with actual value
+    finegrind_learnrate=-0.05,
+    coarsegrind_learnrate=-0.05
+)
+
+# Create the DES context and tie it to the CostGraph
+context = Context(
+    locations_filename=locations_filename,
+    step_costs_filename=step_costs_filename,
+    possible_items=["nacelle", "blade", "tower", "foundation"],
+    cost_graph=netw,
+    cost_graph_update_interval_timesteps=12,
+    avg_blade_masses_filename=avg_blade_masses_filename
+)
+
+# Create the turbine dataframe that will be used to populate
+# the context with components. Repeat the creation of blades
+# 3 times for each turbine.
+
+turbine_data = pd.read_csv(turbine_data_filename)
+components = []
+for _, row in turbine_data.iterrows():
+    year = row['year']
+    blade_mass_tonnes = row['Glass Fiber:Blade']
+    foundation_mass_tonnes = row['foundation_mass_tonnes']
+    facility_id = int(row['facility_id'])
+    n_turbine = int(row['n_turbine'])
+
+    for _ in range(n_turbine):
+        for _ in range(3):
+            components.append({
+                'year': year,
+                'kind': 'blade',
+                'mass_tonnes': blade_mass_tonnes,
+                'facility_id': facility_id
+            })
+
+components = pd.DataFrame(components)
+
+# Create the lifespan functions for the components.
+np.random.seed(13)
+timesteps_per_year = 12
+min_lifespan = 120
+L = 240
+K = 2.2
+lifespan_fns = {
+    "nacelle": lambda: 30 * timesteps_per_year,
+    "blade": lambda: 20 * timesteps_per_year,
+    # "blade": lambda: weibull_min.rvs(K, loc=min_lifespan, scale=L-min_lifespan, size=1)[0],
+    "foundation": lambda: 50 * timesteps_per_year,
+    "tower": lambda: 50 * timesteps_per_year,
+}
+
+# Populate the context with components.
+context.populate(components, lifespan_fns)
+
+# Run the context
+result = context.run()
+
+# Output .csv files of the mass flows of each mass inventory.
+mass_facility_inventories = result["mass_facility_inventories"]
+for facility_name, facility in mass_facility_inventories.items():
+    output_filename = os.path.join(subfolder_dict['outputs_folder'], f'{facility_name}.csv')
+    output_filename = output_filename.replace(' ', '_')
+    facility.transaction_history.to_csv(output_filename, index_label='timestep')
+
+# After PyLCA / DES integration is complete, the next 3 lines should be
+# eliminated
+data_for_lci_filename = os.path.join(subfolder_dict['outputs_folder'], 'data_for_lci.csv')
+data_for_lci_df = pd.DataFrame(context.data_for_lci)
+data_for_lci_df.to_csv(data_for_lci_filename, index=False)
+
+# Plot the cumulative count levels of the inventories
+count_facility_inventory_items = list(result["mass_facility_inventories"].items())
+nrows = 5
+ncols = ceil(len(count_facility_inventory_items) / nrows)
+fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 10))
+plt.tight_layout()
+for i in range(len(count_facility_inventory_items)):
+    subplot_col = i // nrows
+    subplot_row = i % nrows
+    ax = axs[subplot_row][subplot_col]
+    facility_name, facility = count_facility_inventory_items[i]
+    cum_hist_blade = facility.cumulative_history["blade"]
+    ax.set_title(facility_name)
+    ax.plot(range(len(cum_hist_blade)), cum_hist_blade)
+    ax.set_ylabel("tonnes")
+plot_output_path = os.path.join(subfolder_dict['outputs_folder'], 'blade_counts.png')
+plt.savefig(plot_output_path)
