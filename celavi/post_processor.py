4f9e7ebc5cdf30d937877a94706c2d0b637996a0
rjhanes <rebecca.hanes@nrel.gov>
2022-04-01 11:45:40 -0600

    adds post processor file

---
+++ celavi/post_processor.py
@@ -0,0 +1,22 @@
+"""Postprocess and munge raw CELAVI output."""
+
+import pandas as pd
+
+class PostProcessor:
+    """
+
+    """
+
+    def __init__(self,
+                 lcia_locations):
+        """
+        Initialize the post processor.
+
+        Parameters
+        ----------
+        lcia_locations: pd.DataFrame
+            Environmental impact results joined with facility locations.
+        
+        Returns
+        -------
+        """
