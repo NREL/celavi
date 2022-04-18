6fc11c91531dae353c5ea93649e457b41a21402c
rjhanes <rebecca.hanes@nrel.gov>
2022-04-18 14:47:51 -0600

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,7 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+master_doc = "main_page"
 
 # -- Options for HTML output -------------------------------------------------
 
