f828f2eeda6021f7a09537c38dfd9999afdc3352
rjhanes <rebecca.hanes@nrel.gov>
2022-05-16 11:58:20 -0600

    adjusts apply_array_uncertainty method

--- celavi/scenario.py
+++ celavi/scenario.py
@@ -117,12 +117,12 @@ class Scenario:
         print(f"FINISHED RUN at {self.simtime(self.start)} s", flush=True)
 
     @staticmethod
-    def apply_array_uncertainty(run, quantity):
+    def apply_array_uncertainty(quantity, run):
         """Use model run number to access one element in a parameter list."""
         if not isinstance(quantity, list):
-            return quantity
+            return float(quantity)
         else:
-            return quantity[run]
+            return float(quantity[run])
 
     def get_filepaths(self):
         """
