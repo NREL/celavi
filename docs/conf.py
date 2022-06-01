81f275a2d42625e0c0b694b9649ed3898f752dd6
rhanes <rebecca.hanes@nrel.gov>
2022-06-01 11:20:53 -0600

    adjusts sys.path definition

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,8 @@ import sys
 import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("../celavi/"))
-sys.path.insert(0, os.path.abspath("../celavi/pylca_celavi/"))
+sys.path.insert(1, os.path.abspath("../celavi/"))
+sys.path.insert(2, os.path.abspath("../celavi/pylca_celavi/"))
 
 
 # -- Project information -----------------------------------------------------
