df5654ffe4cce074b6dceadc6dbb2c2ee7141d7f
rhanes <rebecca.hanes@nrel.gov>
2022-09-21 14:36:53 -0600

    adds complete docstrings for uncertainty methods

--- celavi/uncertainty_methods.py
+++ celavi/uncertainty_methods.py
@@ -1,7 +1,28 @@
+"""
+Methods for dealing with uncertainty information on parameter values.
+"""
 import scipy.stats as st
 
 def apply_array_uncertainty(quantity, run):
-    """Use model run number to access one element in a parameter list."""
+    """
+    Use model run number to access one element in a parameter list.
+
+    When quantity is a List, then the value of quantity with the index run is returned.
+    When quantity is a Dict, the value assigned to the key "value" is returned.
+    When quantity is a float, it is returned as-is.    
+
+    Parameters
+    ----------
+    quantity: List, Dict, or float
+        A data structure containing a range of parameter values (floats), distribution parameters, or a single float
+
+    run: int
+        The current CELAVI run number
+
+    Returns
+    -------
+    A single float representing the value of quantity during run
+    """
     if isinstance(quantity, list):
         return float(quantity[run])
     elif isinstance(quantity,dict):
@@ -10,7 +31,27 @@ def apply_array_uncertainty(quantity, run):
         return float(quantity)
 
 def apply_stoch_uncertainty(quantity, seed=1, distn=st.triang):
-    """Draw from distribution if parameters exist."""
+    """
+    Draw from distribution if parameters exist.
+
+    When quantity is a Dict, it must contain distribution parameters as well as a key named "value".
+    Once a parameter value has been drawn from the distribution, it is stored under "value".
+    When quantity is a float, it is returned as-is.
+
+    distn defaults to a triangular distribution. If another distribution is used, this method
+    will need to be edited to use the relevant parameters.
+
+    Parameters
+    ----------
+    quantity: Dict or float
+        Contains distribution parameters and a key:value pair for storing the random draw.
+
+    seed: int or an instance of np.random.default_rng
+        Defines the current random state. Must be passed in from Scenario for reproducibility.
+
+    distn: Distribution available in scipy.stats
+        Defaults to the triangular distribution.
+    """
     if isinstance(quantity, dict):
         # return the parameter value drawn from a distribution
         if all([i in quantity.keys() for i in ['c', 'loc', 'scale']]):
