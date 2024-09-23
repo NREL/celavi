61696b2b029d362bada56b025b747f82f26c7334
rhanes <rebecca.hanes@nrel.gov>
2024-09-23 14:16:53 -0500

    adds commercial buildings datafile to ComputeLocations call

--- celavi/scenario.py
+++ celavi/scenario.py
@@ -170,6 +170,7 @@ class Scenario:
             loc = ComputeLocations(
                 start_year=start_year,
                 power_plant_locations=self.files["power_plant_locs"],
+                commercial_building_locations=self.files["commercial_building_locations"],
                 landfill_locations=self.files["landfill_locs"],
                 other_facility_locations=self.files["other_facility_locs"],
                 state_centroids=self.files["state_centroids"],
