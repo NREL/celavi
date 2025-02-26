dd65f9c2b77c69d599297dd2dafcc308d1a353ab
rhanes <rebecca.hanes@nrel.gov>
2025-02-26 13:46:06 -0600

    adds option to scale down the number of components being modeled

--- celavi/scenario.py
+++ celavi/scenario.py
@@ -381,6 +381,15 @@ class Scenario:
         # Create the technology dataframe that will be used to populate
         # the context with components.
         technology_data = pd.read_csv(self.files["technology_data"])
+
+        # If the component_scaledown parameter in scenario.yaml has a value, use it to 
+        # scale down the number of technology components to model
+        # Take the ceiling of the scaled down number (component count should be a round number/integer)
+        # To NOT scale down the number of components, leave the component_scaledown parameter
+        # blank in scenario.yaml
+        if self.scen['scenario']['component_scaledown']:
+            technology_data.loc[:,'n_technology'] = np.ceil((1 / self.scen['scenario']['component_scaledown']) * technology_data.n_technology)
+
         components = []
         for _, row in technology_data.iterrows():
             year = row["year"]
