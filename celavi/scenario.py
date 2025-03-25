356a314e9e01bf3064c265e6e51e91a1c8e776cd
rhanes <rebecca.hanes@nrel.gov>
2025-03-25 11:50:56 -0500

    #240 removes commented out lines prior to update PR

--- celavi/scenario.py
+++ celavi/scenario.py
@@ -395,9 +395,9 @@ class Scenario:
             year = row["year"]
             # @NOTE hard coded to glass study, will need to adapt
             in_use_facility = 'pv in use_' + row["facility_id"] if row['technology'] == 'csi module' else 'window in use_' + row['facility_id']
-            #manuf_facility = self.netw.find_upstream_neighbor(
-            #    row["facility_id"]
-            #)
+            manuf_facility = self.netw.find_upstream_neighbor(
+                row["facility_id"]
+            )
 
             # Optional print statement for component monitoring
             if self.case["model_run"].get("warning_verbose") > 1:
