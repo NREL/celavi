75bacbaf4dc25dd77749d6c173219e1373e5f2f5
rhanes <rebecca.hanes@nrel.gov>
2025-05-28 09:59:59 -0500

    #314 fixes print statement to remove indent

--- celavi/inventory.py
+++ celavi/inventory.py
@@ -128,8 +128,9 @@ class FacilityInventory:
             # This logic should rarely if ever be used, as updates to component.bol_process and
             # component.eol_process should prevent inventories going negative
             print(f"""
-                {self.facility_type}_{self.facility_id} at {timestep=}: Inventory cannot go negative. {quantity} required, {self.component_materials[item_name]} in stock\n
-                Decreasing quantity to {self.component_materials[item_name]}
+{self.facility_type}_{self.facility_id} at {timestep=}: Inventory cannot go negative.
+{quantity} required, {self.component_materials[item_name]}
+in stock\nDecreasing quantity to {self.component_materials[item_name]}
                 """,
                 flush=True
             )
